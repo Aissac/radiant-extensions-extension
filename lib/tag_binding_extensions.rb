eed6aa459e6b9b943a2595e2890177cc5cfa80e3
Cristi Duma <cristi.duma@aissac.ro>
2009-03-13 12:39:52 +0200

    only inject tag parsing code when Radius context is PageContext

--- lib/tag_binding_extensions.rb
+++ lib/tag_binding_extensions.rb
@@ -1,10 +1,20 @@
 module TagBindingExtensions
   def self.included(base)
     base.send(:include, InstanceMethods)
-    base.send(:alias_method, :attr, :parsed_attributes)
+    # base.send(:alias_method, :orig_attr, :attr)
+    # base.send(:alias_method, :attr, :parsed_attributes)
+    base.send(:alias_method_chain, :attr, :parsed_attributes)
   end
   
   module InstanceMethods
+    def attr_with_parsed_attributes
+      if PageContext === context
+        parsed_attributes
+      else
+        attr_without_parsed_attributes
+      end
+    end
+    
     def parsed_attributes
       @parsed_attributes ||= parse_attributes
     end
